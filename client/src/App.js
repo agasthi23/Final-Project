331213b9f0550cfda4f86eb311cc85e8437705c9
agasthi23 <137294353+agasthi23@users.noreply.github.com>
2025-12-28 12:06:37 +0530

    003
    update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard";
+import AddBill from "./pages/AddBill";
+import MainLayout from "./layout/mainlayout";
+import Analytics from "./pages/Analytics";
+import Prediction from "./pages/PredictionPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/add-bill" element={<AddBill />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/predictions" element={<Prediction />} /> 
+        </Routes>
+      </MainLayout>
+    </Router>
   );
 }
 
